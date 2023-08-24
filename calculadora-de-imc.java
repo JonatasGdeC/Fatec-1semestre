34c8dc004b976c1ec5dbb8182d12b80c7718761b
Jônatas <jonatas.07022003@gmail.com>
2023-08-24 17:02:19 -0300

    Add files via upload

---
+++ calculadora-de-imc.java
@@ -0,0 +1,35 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+      //índice de massa comporal (IMC)
+      double IMC = 0;
+      double peso = 200.2;
+      double altura = 2.51;
+      
+      //Calculando um novo valor do IMC
+      IMC = peso / (altura * altura);
+      
+      //Imprimindo o valor do IMC
+      System.out.println("O valor do meu IMC é: " + IMC);
+      
+      /*Classificando o 
+      biotipo 
+      de 
+      uma pessoa*/
+      
+      if(IMC < 18.5){
+          System.out.println("Biotipo = magreza");
+      } else if(IMC >= 18.5 && IMC < 24.9){
+          System.out.println("Biotipo = normal");
+      } else if(IMC >= 24.9 && IMC < 29.9){
+          System.out.println("Biotipo = sobrepeso");
+      } else if(IMC >= 29.9 && IMC < 34.9){
+          System.out.println("Biotipo = obesidade grau 1");
+      } else if(IMC >= 34.9 && IMC <= 40){
+          System.out.println("Biotipo = obesidade grau 2");
+      } else {
+          System.out.println("Biotipo = obesidade grau 3");
+      }
+      
+    }
+}
